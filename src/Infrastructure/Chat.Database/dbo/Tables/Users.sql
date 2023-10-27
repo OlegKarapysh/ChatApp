177d59bd91d4c76aa79d39a7d17e8e4ca33240c1
OlehKarapysh <sebastianvell777@gmail.com>
2023-10-27 22:51:24 +0300

    Added Database and Web projects

---
+++ src/Infrastructure/Chat.Database/dbo/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]    INT            NOT NULL,
+    [Name]  NVARCHAR (MAX) NOT NULL,
+    [Email] NVARCHAR (75)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
