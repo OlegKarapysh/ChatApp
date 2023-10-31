adb58ad6931f813b18ec3bee6823494d82e50762
OlehKarapysh <sebastianvell777@gmail.com>
2023-10-31 18:15:16 +0200

    Added services and httpClient to DI container

---
+++ src/Web/Chat.WebUI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Chat.WebUI.HttpHandlers;
+using Chat.WebUI.Services.Auth;
+
+namespace Chat.WebUI.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddHttpClient(this IServiceCollection services)
+    {
+        services.AddScoped<DelegatingHandler, JwtAuthInterceptor>();
+        services.AddScoped<HttpClient, BlazorHttpClient>();
+    }
+
+    public static void AddCustomServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthWebApiService, AuthWebApiService>();
+        services.AddScoped<IJwtAuthService, JwtAuthService>();
+    }
+}
--- src/Web/Chat.WebUI/Program.cs
+++ src/Web/Chat.WebUI/Program.cs
@@ -1,11 +1,20 @@
-using Chat.WebUI;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
+using Chat.WebUI;
+using Chat.WebUI.Extensions;
+using Chat.WebUI.Providers;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddHttpClient();
+builder.Services.AddCustomServices();
 
 await builder.Build().RunAsync();
