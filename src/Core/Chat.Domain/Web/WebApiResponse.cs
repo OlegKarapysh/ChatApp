bd000b5cae473027df15a1b622e7e73834309792
OlehKarapysh <sebastianvell777@gmail.com>
2023-10-31 18:14:40 +0200

    Added auth DTOs

---
+++ src/Core/Chat.Domain/Web/WebApiResponse.cs
@@ -0,0 +1,10 @@
+﻿using Chat.Domain.DTOs;
+
+namespace Chat.Domain.Web;
+
+public class WebApiResponse<T>
+{
+    public bool IsSuccessful { get; init; }
+    public ErrorDetailsDto? ErrorDetails { get; init; }
+    public T? Content { get; init; }
+}
