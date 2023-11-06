199f5387cecc970fe83bf041151cdfd94976e7c5
OlehKarapysh <sebastianvell777@gmail.com>
2023-11-06 21:14:10 +0200

    Add DTO and enum for people page

---
+++ src/Core/Chat.Domain/DTOs/Users/UsersPagedSearchFilterDto.cs
@@ -0,0 +1,11 @@
+﻿using Chat.Domain.Enums;
+
+namespace Chat.Domain.DTOs.Users;
+
+public class UsersPagedSearchFilterDto
+{
+    public int Page { get; set; } = 1;
+    public string SearchFilter { get; set; } = string.Empty;
+    public string SortingProperty { get; set; } = string.Empty;
+    public SortingOrder SortingOrder { get; set; } = SortingOrder.Ascending;
+}
