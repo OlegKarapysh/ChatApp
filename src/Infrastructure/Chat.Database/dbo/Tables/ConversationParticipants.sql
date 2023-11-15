4a479c176bda0c04b31393bedcae49ad9f4e6026
OlehKarapysh <sebastianvell777@gmail.com>
2023-11-15 17:53:56 +0200

    Add conversation participants table

---
+++ src/Infrastructure/Chat.Database/dbo/Tables/ConversationParticipants.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[ConversationParticipants] (
+    [Id]             INT IDENTITY (1, 1) NOT NULL,
+    [ConversationId] INT NOT NULL,
+    [UserId]         INT NOT NULL,
+    [MembershipType] INT NOT NULL,
+    CONSTRAINT [PK_ConversationParticipants] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_ConversationParticipants_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[AspNetUsers] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_ConversationParticipants_Conversations_ConversationId] FOREIGN KEY ([ConversationId]) REFERENCES [dbo].[Conversations] ([Id]) ON DELETE CASCADE
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ConversationParticipants_UserId]
+    ON [dbo].[ConversationParticipants]([UserId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_ConversationParticipants_ConversationId]
+    ON [dbo].[ConversationParticipants]([ConversationId] ASC);
+
