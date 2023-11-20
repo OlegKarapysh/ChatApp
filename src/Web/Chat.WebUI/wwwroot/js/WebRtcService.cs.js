3b8ef3db3912c6b7d2496c3aa4cceb5055ee3988
OlehKarapysh <sebastianvell777@gmail.com>
2023-11-20 16:31:05 +0200

    Enabled sound

--- src/Web/Chat.WebUI/wwwroot/js/WebRtcService.cs.js
+++ src/Web/Chat.WebUI/wwwroot/js/WebRtcService.cs.js
@@ -1,13 +1,12 @@
 ﻿"use strict";
 // Set up media stream constant and parameters.
 const mediaStreamConstraints = {
-    video: true
-    //audio: true
+    video: true,
+    audio: true
 };
-// Set up to exchange only video.
 const offerOptions = {
-    offerToReceiveVideo: 1
-    //offerToReceiveAudio: 1
+    offerToReceiveVideo: 1,
+    offerToReceiveAudio: 1
 };
 const servers = {
     iceServers: [
