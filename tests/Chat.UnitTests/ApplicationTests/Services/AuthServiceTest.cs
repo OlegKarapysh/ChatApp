aef7565b916947a947134253a96cc0c553430935
OlehKarapysh <sebastianvell777@gmail.com>
2023-11-22 18:11:00 +0200

    Add unit tests for JwtService and some AuthController actions

---
+++ tests/Chat.UnitTests/ApplicationTests/Services/AuthServiceTest.cs
@@ -0,0 +1,6 @@
+﻿namespace Chat.UnitTests.ApplicationTests.Services;
+
+public class AuthServiceTest
+{
+    
+}
--- tests/Chat.UnitTests/GlobalUsings.cs
+++ tests/Chat.UnitTests/GlobalUsings.cs
@@ -1,1 +1,15 @@
+global using System.Text.RegularExpressions;
+global using Microsoft.Extensions.Options;
 global using Xunit;
+global using Moq;
+global using FluentAssertions;
+global using Chat.WebAPI.Controllers;
+global using Chat.Application.JWT;
+global using Chat.Application.Services.JWT;
+global using System.Net;
+global using Chat.Application.Services.Authentication;
+global using Chat.Domain.DTOs.Authentication;
+global using Microsoft.AspNetCore.Mvc;
+global using System.Security.Claims;
+global using Chat.Application.RequestExceptions;
+
