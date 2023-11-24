2b0c8ba1aa46791746a26e2702b4b5ce794d9ee7
OlehKarapysh <sebastianvell777@gmail.com>
2023-11-24 17:36:54 +0200

    Add MockHelper and TestDataGenerator

---
+++ tests/Chat.UnitTests/TestHelpers/MockHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Chat.UnitTests.TestHelpers;
+
+public static class MockHelper
+{
+    public static Mock<UserManager<User>> MockUserManager()
+    {
+        var store = new Mock<IUserStore<User>>();
+        var mock = new Mock<UserManager<User>>(store.Object, null!, null!, null!, null!, null!, null!, null!, null!);
+        mock.Object.UserValidators.Add(new UserValidator<User>());
+        mock.Object.PasswordValidators.Add(new PasswordValidator<User>());
+        return mock;
+    }
+
+    public static Mock<SignInManager<User>> MockSignInManager(UserManager<User> userManager)
+    {
+        return new Mock<SignInManager<User>>(userManager, Mock.Of<IHttpContextAccessor>(),
+            Mock.Of<IUserClaimsPrincipalFactory<User>>(), null!, null!, null!, null!);
+    }
+}
