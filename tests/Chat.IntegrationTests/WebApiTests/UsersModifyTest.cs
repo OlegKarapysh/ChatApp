3287dcc4cd971bd5e39e5cabb6e9b79275cd33e8
OlehKarapysh <sebastianvell777@gmail.com>
2023-11-28 19:39:32 +0200

    Add update test

---
+++ tests/Chat.IntegrationTests/WebApiTests/UsersModifyTest.cs
@@ -0,0 +1,23 @@
+﻿namespace Chat.IntegrationTests.WebApiTests;
+
+[Collection("Sequential")]
+public class UsersModifyTest : IClassFixture<IntegrationTest>
+{
+    private readonly IntegrationTest _test;
+    
+    public UsersModifyTest(IntegrationTest test)
+    {
+        _test = test;
+    }
+
+    [Fact]
+    public async Task Update_UpdatesUser()
+    {
+        // Arrange.
+        
+        // Act.
+
+        // Assert.
+    }
+    
+}
