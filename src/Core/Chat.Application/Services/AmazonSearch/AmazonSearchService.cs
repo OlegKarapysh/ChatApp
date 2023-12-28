070b653f9821b50f35862cfb55ded42d8b91c682
OlehKarapysh <sebastianvell777@gmail.com>
2023-12-28 09:10:12 +0200

    Changed parsing

--- src/Core/Chat.Application/Services/AmazonSearch/AmazonSearchService.cs
+++ src/Core/Chat.Application/Services/AmazonSearch/AmazonSearchService.cs
@@ -5,7 +5,7 @@ namespace Chat.Application.Services.AmazonSearch;
 public sealed class AmazonSearchService : IAmazonSearchService
 {
     // TODO: create assistant for each user.
-    private const string AmazonAssistantId = "asst_2cVSz0i6XuiCVmYc8zKcJADW";
+    private const string AmazonAssistantId3 = "asst_P4WTCXC6aAPGcwYgJLI6iUu2";
     private const string SearchResultAttributeName = "data-component-type";
     private const string SearchResultAttributeValue = "s-search-result";
     private readonly HttpClient _httpClient;
@@ -23,11 +23,10 @@ public sealed class AmazonSearchService : IAmazonSearchService
         var response = await _httpClient.GetAsync($"s?k={name}");
         // TODO: detect encoding and compression dynamically.
         var htmlText = await ParseHtmlTextAsync(response);
-        var searchResultDivs = GetSearchResultsDivs(htmlText).ToArray();
-        LogHtml(searchResultDivs.First());
-        // TODO: remove Take() call.
-        var functionCallTasks = searchResultDivs.Take(2).Select(searchResult =>
-            _openAiService.GetFunctionCallArgsAsync<AmazonProductDto>(searchResult, AmazonAssistantId)).ToList();
+        var searchResultDivs = ParseSearchResults(htmlText);
+        searchResultDivs.Take(2).ForEach(x => LogHtml(x));
+        var functionCallTasks = searchResultDivs.Take(5).Select(searchResult =>
+            _openAiService.GetFunctionCallArgsAsync<AmazonProductDto>(searchResult, AmazonAssistantId3)).ToList();
         await Task.WhenAll(functionCallTasks);
         
         return functionCallTasks.Where(t => t.Result is not null).Select(t => t.Result!).ToList();
@@ -40,20 +39,52 @@ public sealed class AmazonSearchService : IAmazonSearchService
         using var reader = new StreamReader(decompressedStream, Encoding.UTF8);
         return await reader.ReadToEndAsync();
     }
-
-    private IEnumerable<string> GetSearchResultsDivs(string htmlText)
+    
+    private string[] ParseSearchResults(string htmlContent)
     {
-        var htmlDoc = new HtmlDocument();
-        htmlDoc.LoadHtml(htmlText);
-        if (htmlDoc.DocumentNode is null)
+        var doc = new HtmlDocument();
+        doc.LoadHtml(htmlContent);
+        var searchResultDivs = doc.DocumentNode.SelectNodes("//div[@data-component-type='s-search-result']");
+        if (searchResultDivs is null)
         {
-            throw new Exception("Couldn't parse document node from HTML");
+            return Array.Empty<string>();
         }
-        
-        return htmlDoc.DocumentNode
-                      .Descendants("div")
-                      .Where(div => div.GetAttributeValue(SearchResultAttributeName, string.Empty) == SearchResultAttributeValue)
-                      .Select(div => div.InnerHtml);
+
+        var results = new List<string>();
+        foreach (var div in searchResultDivs)
+        {
+            var filteredContents = new List<string>();
+            var imgTags = div.SelectNodes(".//img");
+            if (imgTags is not null)
+            {
+                foreach (var img in imgTags)
+                {
+                    var src = img.GetAttributeValue("src", "");
+                    if (!string.IsNullOrEmpty(src))
+                    {
+                        filteredContents.Add($"<img src=\"{src}\">");
+                    }
+                }
+            }
+            var textContainingElements = div.SelectNodes(".//*[not(self::script) and not(self::style) and normalize-space(text())]");
+            if (textContainingElements is not null)
+            {
+                foreach (var element in textContainingElements)
+                {
+                    var classAttr = element.GetAttributeValue("class", null!);
+                    var classAttribute = classAttr is not null ? $"class=\"{classAttr}\"" : "";
+                    var cleanElement = HtmlNode.CreateNode($"<{element.Name} {classAttribute}>{element.InnerText}</{element.Name}>");
+                    filteredContents.Add(cleanElement.OuterHtml);
+                }
+            }
+            
+            var content = string.Join(' ', filteredContents);
+            content = content.Replace("<span", "<p").Replace("</span>", "</p>");
+            content = content.Replace("<div", "<p").Replace("</div>", "</p>");
+            results.Add(content.Trim());
+        }
+
+        return results.ToArray();
     }
 
     private void LogHtml(string html, string path = @"C:\Users\sebas\OneDrive\Desktop\productsHtml.txt")
